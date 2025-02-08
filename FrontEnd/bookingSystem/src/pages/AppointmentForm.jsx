cf082ca7fc74d7afa85e105da7f972bf8b726da5
harshani <ruwanthikapemarathna@gmail.com>
2025-02-08 21:56:22 +0530

    Completed Appointment Form

--- FrontEnd/bookingSystem/src/pages/AppointmentForm.jsx
+++ FrontEnd/bookingSystem/src/pages/AppointmentForm.jsx
@@ -1,19 +1,59 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const AppointmentForm = () => {
+  const [name, setName] = useState("");
+  const [contact, setContact] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+
+  const navigate = useNavigate();
+
+  const saveAppointment = async (e) => {
+    e.preventDefault();
+
+    if (!name || !contact || !time || !date) {
+      alert("All fields are required!");
+      return;
+    }
+
+    const formattedTime = time.length === 2 ? `${time}:00:00` : time;
+    try {
+      const response = await axios.post(
+        "http://localhost:8081/api/v1/appointments",
+        {
+          name,
+          contact,
+          date,
+          time: formattedTime,
+        }
+      );
+      alert("Appointment added successfully!");
+      console.log("Response:", response.data);
+      navigate("/appointment-list");
+    } catch (error) {
+      console.error("Error:", error.response?.data || error.message);
+    }
+  };
+
   return (
     <>
-      <div className="max-w-md mx-auto bg-white p-6 rounded-2xl shadow-lg border border-gray-200 mt-5">
+      <div className="max-w-md mx-auto bg-white p-5 rounded-2xl shadow-lg border border-gray-200 mt-4">
         <h2 className="text-3xl font-bold text-cyan-800 mb-6 text-center">
           Book Your Appointment
         </h2>
 
-        <form className="space-y-4">
+        <form className="space-y-3">
           <div>
             <label className="block text-gray-700 font-medium">Name</label>
             <input
               type="text"
+              value={name}
               className="w-full mt-1 p-3 border rounded-lg shadow-sm transition duration-200"
               placeholder="Enter your name"
               required
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
 
@@ -23,11 +63,12 @@ const AppointmentForm = () => {
             </label>
             <input
               type="tel"
+              value={contact}
               className="w-full mt-1 p-3 border rounded-lg shadow-sm transition duration-200"
               placeholder="Enter your phone number"
               pattern="\d{10}"
-              title="Enter a valid 10-digit number"
               required
+              onChange={(e) => setContact(e.target.value)}
             />
           </div>
 
@@ -35,23 +76,35 @@ const AppointmentForm = () => {
             <label className="block text-gray-700 font-medium">Date</label>
             <input
               type="date"
+              value={date}
               className="w-full mt-1 p-3 border rounded-lg shadow-sm transition duration-200"
               required
+              onChange={(e) => setDate(e.target.value)}
             />
           </div>
 
           <div>
             <label className="block text-gray-700 font-medium">Time</label>
             <input
-              type="time"
+              type="number"
+              value={time}
               className="w-full mt-1 p-3 border rounded-lg shadow-sm transition duration-200"
+              min="10"
+              max="17"
+              step="1"
+              placeholder="Enter hour 10AM - 16PM"
               required
+              onChange={(e) => setTime(e.target.value)}
             />
           </div>
+          {/* <span className="text-blue-700 font-medium underline flex flex-row justify-end items-end">
+            <Link to={"/calander"}> Calender</Link>
+          </span> */}
 
           <button
             type="submit"
-            className="w-full py-2 bg-gradient-to-r from-cyan-500 to-blue-500 text-white font-semibold rounded-lg shadow-md hover:from-cyan-600 hover:to-blue-600 transition duration-300 transform hover:scale-105"
+            className="w-full py-3 bg-gradient-to-r from-cyan-500 to-blue-500 text-white font-semibold rounded-lg shadow-md hover:from-cyan-600 hover:to-blue-600 transition duration-300 transform hover:scale-105"
+            onClick={saveAppointment}
           >
             Confirm Appointment
           </button>
